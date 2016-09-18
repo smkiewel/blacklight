6496e419756c9a6c9226a981d475b504fdecfcc5
Justin Coyne <jcoyne@justincoyne.com>
2016-09-18 15:34:51 -0400

    Move helper methods to presenters

--- app/helpers/blacklight/catalog_helper_behavior.rb
+++ app/helpers/blacklight/catalog_helper_behavior.rb
@@ -280,10 +280,10 @@ module Blacklight::CatalogHelperBehavior
     facet_config = facet_configuration_for_field(facet)
     filter_label = facet_field_label(facet_config.key)
     filter_value = if values.size < 3
-      values.map {|value| facet_display_value(facet, value)}.to_sentence
-    else 
-      t('blacklight.search.page_title.many_constraint_values', values: values.size)
-    end
+                     values.map { |value| Blacklight::FacetValuePresenter.new(facet, value, self).display }.to_sentence
+                   else 
+                     t('blacklight.search.page_title.many_constraint_values', values: values.size)
+                   end
     t('blacklight.search.page_title.constraint', label: filter_label, value: filter_value)
   end
 
